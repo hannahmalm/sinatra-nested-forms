f76475af2fde428cd885d866e10ad135ee7d486d
Hannah Malm <hannahemalm616@gmail.com>
2021-08-22 12:44:30 -0400

    commit tree 6d6e4a114b8a2007c539100208a120d76ae30d6a

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+    attr_reader :name, :weight, :height
+
+    #create an empty array to hold pirate information
+    pirates = []
+
+    #add arguments to instance variables and then put the newly instantiated pirate into the array
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        pirates << self
+    end 
+
+    #list out the pirates
+    def self.all
+        pirates 
+    end 
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+    attr_reader :name, :type, :booty 
+    #attr_reader means that you cannot write/ create so these wont change
+
+    #create a blank array with a class variable
+    @@ships = []
+
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+        @@ships << self 
+    end 
+
+    def self.all
+        @@ships 
+    end 
+
+    def self.clear 
+        @@ships = []
+    end 
+
+
 end
